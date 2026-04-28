a55ad42fe1383952aeb8845f20e94a2ee33b23f1
Brice-kge <briice004@gmail>
2026-04-28 17:20:56 +0100

    schema prisma cree v2.2

---
+++ prisma/migrations/20260428150127_init/migration.sql
@@ -0,0 +1,146 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ELECTEUR', 'ADMIN', 'SUPER_ADMIN', 'OBSERVATEUR');
+
+-- CreateEnum
+CREATE TYPE "StatutElection" AS ENUM ('CREATED', 'OPEN', 'CLOSED', 'PUBLISHED');
+
+-- CreateEnum
+CREATE TYPE "StatutInscription" AS ENUM ('EN_ATTENTE', 'VALIDE', 'REJETE');
+
+-- CreateTable
+CREATE TABLE "Utilisateur" (
+    "id" TEXT NOT NULL,
+    "nom" TEXT NOT NULL,
+    "prenom" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "motDePasse" TEXT NOT NULL,
+    "telephone" TEXT NOT NULL,
+    "matricule" TEXT NOT NULL DEFAULT '',
+    "role" "Role" NOT NULL,
+    "institutionId" TEXT NOT NULL,
+    "electionId" TEXT NOT NULL,
+    "inscriptionId" TEXT NOT NULL,
+
+    CONSTRAINT "Utilisateur_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Institution" (
+    "id" TEXT NOT NULL,
+    "nom" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "pays" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "telephone" TEXT NOT NULL,
+    "utilisateurid" TEXT NOT NULL,
+    "electionId" TEXT NOT NULL,
+
+    CONSTRAINT "Institution_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Election" (
+    "id" TEXT NOT NULL,
+    "titre" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "dateDebut" TIMESTAMP(3) NOT NULL,
+    "dateFin" TIMESTAMP(3) NOT NULL,
+    "statut" "StatutElection" NOT NULL,
+    "candidatid" TEXT NOT NULL,
+    "inscriptionid" TEXT NOT NULL,
+    "voteid" TEXT NOT NULL,
+
+    CONSTRAINT "Election_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Candidat" (
+    "id" TEXT NOT NULL,
+    "nom" TEXT NOT NULL,
+    "prenom" TEXT NOT NULL,
+    "programme" TEXT NOT NULL,
+    "photo" TEXT,
+    "voteid" TEXT NOT NULL,
+
+    CONSTRAINT "Candidat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Inscription" (
+    "id" TEXT NOT NULL,
+    "dateInscription" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "statut" "StatutInscription" NOT NULL,
+
+    CONSTRAINT "Inscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Vote" (
+    "id" TEXT NOT NULL,
+    "dateVote" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "utilisateurId" TEXT NOT NULL,
+
+    CONSTRAINT "Vote_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Resultat" (
+    "id" TEXT NOT NULL,
+    "totalVotes" INTEGER NOT NULL,
+    "tauxParticipation" DOUBLE PRECISION NOT NULL,
+    "electionId" TEXT NOT NULL,
+    "candidatid" TEXT NOT NULL,
+
+    CONSTRAINT "Resultat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Utilisateur_email_key" ON "Utilisateur"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Utilisateur_telephone_key" ON "Utilisateur"("telephone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Utilisateur_matricule_institutionId_key" ON "Utilisateur"("matricule", "institutionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Institution_utilisateurid_key" ON "Institution"("utilisateurid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Resultat_electionId_key" ON "Resultat"("electionId");
+
+-- AddForeignKey
+ALTER TABLE "Utilisateur" ADD CONSTRAINT "Utilisateur_institutionId_fkey" FOREIGN KEY ("institutionId") REFERENCES "Institution"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Utilisateur" ADD CONSTRAINT "Utilisateur_electionId_fkey" FOREIGN KEY ("electionId") REFERENCES "Election"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Utilisateur" ADD CONSTRAINT "Utilisateur_inscriptionId_fkey" FOREIGN KEY ("inscriptionId") REFERENCES "Inscription"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Institution" ADD CONSTRAINT "Institution_utilisateurid_fkey" FOREIGN KEY ("utilisateurid") REFERENCES "Utilisateur"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Institution" ADD CONSTRAINT "Institution_electionId_fkey" FOREIGN KEY ("electionId") REFERENCES "Election"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Election" ADD CONSTRAINT "Election_candidatid_fkey" FOREIGN KEY ("candidatid") REFERENCES "Candidat"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Election" ADD CONSTRAINT "Election_inscriptionid_fkey" FOREIGN KEY ("inscriptionid") REFERENCES "Inscription"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Election" ADD CONSTRAINT "Election_voteid_fkey" FOREIGN KEY ("voteid") REFERENCES "Vote"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Candidat" ADD CONSTRAINT "Candidat_voteid_fkey" FOREIGN KEY ("voteid") REFERENCES "Vote"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_utilisateurId_fkey" FOREIGN KEY ("utilisateurId") REFERENCES "Utilisateur"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Resultat" ADD CONSTRAINT "Resultat_electionId_fkey" FOREIGN KEY ("electionId") REFERENCES "Election"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Resultat" ADD CONSTRAINT "Resultat_candidatid_fkey" FOREIGN KEY ("candidatid") REFERENCES "Candidat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
